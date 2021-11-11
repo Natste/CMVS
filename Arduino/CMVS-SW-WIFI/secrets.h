c1e892c814265b1b59dfec839b9eb9baa58082f9
Stephen Natale <Natsebe@gmail.com>
2021-11-11 14:38:36 -0500

    wifi updates

--- Arduino/CMVS-SW-WIFI/secrets.h
+++ Arduino/CMVS-SW-WIFI/secrets.h
@@ -2,10 +2,12 @@
 // and connection details
 #ifndef SECRETS_H
 #define SECRETS_H
-#define SECRET_SSID ""
-#define SECRET_PASS ""
-#define WPI_SSID "WPI-Open"
-#define WPI_PASS "\0"
+#define SECRET_SSID "iPhone"
+#define SECRET_PASS "wet$paghetti1979"
+#define WPI_SSID "iPhone"
+#define WPI_PASS "wet$paghetti1979"
+//#define WPI_SSID "WPI-Open"
+//#define WPI_PASS "\0"
 
 #define SECRET_CH_ID 1552033
 #define SECRET_WRITE_APIKEY "AD8ZB04MFD6HIYI8"
